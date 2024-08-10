ebfdd998c8046ef702fea664e1425d02ed444b9e
N1ckon6 <denis.nickon2@gmail.com>
2024-08-10 23:52:18 +0300

    bug fixed

--- src/components/BannerHome.js
+++ src/components/BannerHome.js
@@ -83,7 +83,7 @@ const BannerHome = () => {
                     <span>|</span>
                     <p>View: {Number(data.popularity).toFixed(0)}</p>
                   </div>
-                  <Link to={"/" + data.media_type + "/" + "data.id"}>
+                  <Link to={"/" + data.media_type + "/" + data.id}>
                     <button
                       className=" bg-white px-4 py-2 text-black font-bold rounded mt-4
                   hover:bg-gradient-to-l from-red-700 to-orange-500 shadow-md transition-all 
